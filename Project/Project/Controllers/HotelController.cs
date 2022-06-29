b0cd605c08c8c679261ed3a2c636aebc28d905b1
Nahid Hasan <65039500+Nahid-hub@users.noreply.github.com>
2022-06-30 03:45:02 +0600

    3rd Commit

---
+++ Project/Project/Controllers/HotelController.cs
@@ -0,0 +1,43 @@
+﻿using Project.Models.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Controllers
+{
+    public class HotelController : Controller
+    {
+        // GET: Hotel
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registration(Registration r)
+        {
+            if(ModelState.IsValid){
+                ProjectEntities db = new ProjectEntities();
+                db.Registrations.Add(r);
+                db.SaveChanges();
+                return RedirectToAction("Login");
+
+            }
+            return View();
+        }
+    }
+
+    public class Hotel
+    {
+    }
+}
